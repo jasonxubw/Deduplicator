98edc8249a65fad575219914d6c88cf4cb5cf065
jasonxubw <jasonxubw@gmail.com>
2019-04-22 15:59:14 -0400

    Update MyFile.java
    Added additional MyFile class properties.

--- Deduplicator/src/Dedupe/MyFile.java
+++ Deduplicator/src/Dedupe/MyFile.java
@@ -1,18 +1,25 @@
 package Dedupe;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 
 public class MyFile{
 
     private String fileName;
     private ArrayList<String> fileRetriever;
     private String hashOfFile;
+    private final LinkedHashSet<String> hs;
+    private final long originalFileSize;
+    private long actualStoredSize;
 //    private long size;
 
-    public MyFile(String fileName, ArrayList<String> fileRetriever, String hashOfFile) {
+    public MyFile(String fileName, ArrayList<String> fileRetriever, String hashOfFile, LinkedHashSet<String> hs, long originalFileSize, long actualStoredSize) {
         this.fileName = fileName;
         this.fileRetriever = fileRetriever;
         this.hashOfFile = hashOfFile;
+        this.hs = hs;
+        this.originalFileSize = originalFileSize;
+        this.actualStoredSize = actualStoredSize;
 //        this.size = size;
     }
 
@@ -26,5 +33,17 @@ public class MyFile{
 
     protected String getHashOfFile(){
         return this.hashOfFile;
+    }
+
+    protected LinkedHashSet<String> getHashSet(){
+        return this.hs;
+    }
+
+    protected long getOriginalFileSize(){
+        return this.originalFileSize;
+    }
+
+    protected long getActualStoredSize(){
+        return this.actualStoredSize;
     }
 }
