7ecaa8c4ae1fcbaf2eaf51711e34b592e37190bb
jasonxubw <jasonxubw@gmail.com>
2019-04-22 16:04:40 -0400

    Update HashTheChunks.java
    Added Dynamic chunking feature

--- Deduplicator/src/Dedupe/HashTheChunks.java
+++ Deduplicator/src/Dedupe/HashTheChunks.java
@@ -11,6 +11,8 @@ import java.nio.file.Path;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
 
 public class HashTheChunks {
     // this function returns a file type as a string
@@ -50,11 +52,72 @@ public class HashTheChunks {
     }
 
     public int getChunksize(String extension, int fileSize) {
-        int chunkSize = 128;
+        int chunkSize = 2048;
+        if(isVideo(extension) || isPDF(extension)){
+            if (fileSize > 50000000){  //50mb
+                chunkSize = 8192;
+            }
+            else if(fileSize > 10000000){ //10mb
+                chunkSize = 4096;
+            }
+            else if(fileSize > 1000000){ //1mb
+                chunkSize = 2048;
+            }
+            else{
+                chunkSize = 512;
+            }
+        }
+        if((isImage(extension) || isTextFile(extension))){
+            if (fileSize > 10000000){ //10mb
+                chunkSize = 2048;
+            }
+            else if(fileSize > 1000000){ //1mb
+                chunkSize = 1024;
+            }
+            else{
+                chunkSize = 512;
+            }
+        }
         return chunkSize;
     }
 
-    public String fixedSizeChunk(String filePath, int chunkSize, MyLocker locker) throws NoSuchAlgorithmException, IOException {
+    public boolean isVideo(String extension){
+        if(extension.equalsIgnoreCase(".wav") ||
+           extension.equalsIgnoreCase(".mov") ||
+           extension.equalsIgnoreCase(".mp4") ||
+           extension.equalsIgnoreCase(".flv") ||
+           extension.equalsIgnoreCase(".avi") ) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isImage(String extension){
+        if(extension.equalsIgnoreCase(".png") ||
+           extension.equalsIgnoreCase(".jpeg") ||
+           extension.equalsIgnoreCase(".gif")) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isTextFile(String extension){
+        if(extension.equalsIgnoreCase(".txt") ||
+           extension.equalsIgnoreCase(".docx") ||
+           extension.equalsIgnoreCase(".rtf")) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isPDF(String extension){
+        if(extension.equalsIgnoreCase(".pdf")){
+            return true;
+        }
+        return false;
+    }
+
+    public void fixedSizeChunk(String filePath, int chunkSize, MyLocker locker) throws NoSuchAlgorithmException, IOException {
         FileInputStream fis = null;
         ArrayList<String> fileRetriever = new ArrayList<>();
 
@@ -62,6 +125,11 @@ public class HashTheChunks {
         String fileName = file.getName();
         Path p = FileSystems.getDefault().getPath(filePath);
         byte[] fileBytes = Files.readAllBytes(p);
+        LinkedHashSet<String> hs = new LinkedHashSet<>();
+        long sizeInsertedInLocker = 0L;
+        long originalFileSize = fileBytes.length;
+        long actualStoredSize = 0;
+        locker.addToOriginalFileSize(originalFileSize);
 
         String hashOfFile = hashContent(fileBytes);
 
@@ -77,12 +145,16 @@ public class HashTheChunks {
 
             String hashOutput = hashContent(chunk);
             currChunkPosition += chunkSize;
-            locker.storeChunktoChunkLocker(hashOutput, chunk); //stores to chunkLocker the hash and its chunk
+
+            if(!locker.containsHashInChunkLocker(hashOutput)){
+                locker.storeChunktoChunkLocker(hashOutput, chunk); //stores to chunkLocker the hash and its chunk
+                sizeInsertedInLocker += chunk.length;
+                actualStoredSize += chunk.length;
+            }
+
             fileRetriever.add(hashOutput);
+            hs.add(hashOutput);
 
-
-
-            System.out.println("Each chunk's length: " + chunk.length + "  &&   Our chunk's hash output: " + hashOutput);
             baos.reset();
 
             // If there is enough unprocessed data left in file to fill a full chunk
@@ -90,12 +162,107 @@ public class HashTheChunks {
                 chunkSize = fileBytes.length - currChunkPosition;
             }
         }
-        locker.storeFileToMyLocker(fileName, fileRetriever, hashOfFile);
-        System.out.println("Size of locker: " + locker.getSizeOfLocker());
-        System.out.println("Size of chunk locker: " + locker.getSizeOfChunkLocker());
+        locker.storeFileToMyLocker(fileName, fileRetriever, hashOfFile, hs, originalFileSize, actualStoredSize);
+        locker.addSizeToChunkLocker(sizeInsertedInLocker);
+        System.out.println("Size of file locker: " + locker.getSizeOfFileLocker());
+        System.out.println("Size of chunk locker: " + locker.getNumOfChunksInChunkLocker());
         System.out.println("Name of my locker: " + locker.getNameOfLocker());
+        System.out.println("Size of entire locker: " + (locker.getSizeOfChunkLocker() / 1000) + "kb");
 
-        return "";
+        return;
+    }
+
+    public void dynamicSizeChunk(String filePath, int chunkSize, MyLocker locker) throws IOException, NoSuchAlgorithmException{
+        File file = new File(filePath);
+        String fileName = file.getName();
+        Path p = FileSystems.getDefault().getPath(filePath);
+        byte[] fileBytes = Files.readAllBytes(p);
+        ArrayList<String> fileRetriever = new ArrayList<>(); //stores hashes of chunks so file could be retrieved later on
+        LinkedHashSet<String> hashSet = new LinkedHashSet<>();
+        long originalFileSize = fileBytes.length;
+        long sizeInsertedInLocker = 0;
+        long actualStoredSize = 0;
+        locker.addToOriginalFileSize(originalFileSize);
+
+        String hashOfFile = hashContent(fileBytes);
+
+        int largePrimeNumber = 69031;
+        int fileBytesLength = fileBytes.length; // length of the file bytes array
+        int rkWindowIndex = 0; //keep track of the rabin karp window's index
+        int currRKWindowStartIndex = 0;
+        int currRKWindowSize = chunkSize;
+        byte[] rkWindow = new byte[currRKWindowSize];
+        int rkWindowSize = rkWindow.length;
+        int polynomial = 1; //polynomial multiplier
+        int fileHashIndex = 0; //keep track of the file bytes array index as we iterate through
+        int currHashVal = 0; //the hash of the chunked bytes ; will update as rabin karp window shifts
+        int fileBytesCounter = 0; //keep track of how many bytes we've iterated through in file bytes array
+        boolean initialChunk = true;
+
+        int mask = decideMask(chunkSize);
+
+        byte[] chunk;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        //Initialize the rabin karp hash window with the first chunk of bytes
+        for(int i = 0; i < chunkSize; i++){
+            byte currFileHashByte = fileBytes[fileHashIndex];
+            fileHashIndex++;
+            if(!initialChunk){
+                currHashVal *= largePrimeNumber;
+                currHashVal += currFileHashByte; //add next hash
+                polynomial *= largePrimeNumber;
+
+            }
+            else{
+                currHashVal += currFileHashByte;
+                initialChunk = false;
+            }
+            rkWindow[rkWindowIndex] = currFileHashByte;
+            rkWindowIndex++;
+            rkWindowIndex = rkWindowIndex % rkWindowSize;
+            baos.write(currFileHashByte);
+            fileBytesCounter++;
+        }
+
+        while(currRKWindowStartIndex + currRKWindowSize < fileBytesLength){
+
+            if(fileBytesCounter <= fileBytesLength){
+                byte currFileHashByte = fileBytes[fileHashIndex];
+                fileHashIndex++;
+                int temp = polynomial * rkWindow[rkWindowIndex];
+                currHashVal = currHashVal - temp; //remove the value of hash at beginning of window
+                currHashVal *= largePrimeNumber;
+                currHashVal += currFileHashByte;
+                rkWindow[rkWindowIndex] = currFileHashByte;
+                rkWindowIndex++;
+                rkWindowIndex = rkWindowIndex % rkWindowSize;
+                baos.write(currFileHashByte);
+                fileBytesCounter++;
+            }
+
+            currRKWindowSize++;
+
+            if((currHashVal & mask) == 0 || fileBytesCounter == fileBytesLength){
+                chunk = baos.toByteArray();
+                baos.reset();
+                String hashOutput = hashContent(chunk);
+
+                currRKWindowStartIndex += currRKWindowSize;
+                currRKWindowSize = 0;
+
+                if(!locker.containsHashInChunkLocker(hashOutput)) {
+                    locker.storeChunktoChunkLocker(hashOutput, chunk);
+                    sizeInsertedInLocker += chunk.length;
+                    actualStoredSize += chunk.length;
+                }
+                fileRetriever.add(hashOutput);
+                hashSet.add(hashOutput);
+                }
+
+        }
+        locker.storeFileToMyLocker(fileName,fileRetriever, hashOfFile, hashSet, originalFileSize, actualStoredSize);
+        locker.addSizeToChunkLocker(sizeInsertedInLocker);
     }
 
     public String hashContent(byte[] content) throws NoSuchAlgorithmException, IOException {
@@ -108,5 +275,26 @@ public class HashTheChunks {
         }
         String hashedFileString = sb.toString();
         return hashedFileString;
+    }
+
+    public int decideMask(int chunkSize){
+        int mask;
+        if(chunkSize == 512){
+            mask = 1 << 9;
+        }
+        else if(chunkSize == 2048){
+            mask = 1 << 11;
+        }
+        else if(chunkSize == 4096){
+            mask = 1 << 12;
+        }
+        else if(chunkSize == 8192){
+            mask = 1 << 13;
+        }
+        else {
+            mask = 1 << 12;
+        }
+        mask -= 1;
+        return mask;
     }
 }
